9b8012ab4a7ea73e216816e6d72b7cc1fb6ba25c
Prathmesh Khamkar <prathmeshkhamkar19>
2026-03-07 13:48:16 +0530

    We write code in app.py that make session for nmae and email and slo call the registration.html by passing the boject (obj_class) as refrence of form if condition is not satisfied

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask,request,render_template,url_for,Response,redirect,session
+from form import Datainput  #from form Module import the Datainput class
+
+app =Flask(__name__)
+app.secret_key= "project-secreate-key"
+
+@app.route("/",methods=["POST","GET"]) #this is page firstly open(defult)
+def home():  # in that page using from (flask -wtf) we get the data from user 
+ obj_Class= Datainput()  
+ #creating object (obj_class ) of class Datapoint()
+ if obj_Class.validate_on_submit(): 
+    name= obj_Class.name.data
+    email= obj_Class.email_id.data #geting the email data(email_id(variable of flask) to email (variable of email))
+      #to get data mindit the object name
+
+     # Store in session so we can use them on other pages
+    session["name"] = name
+    session["email"] = email
+    
+    return redirect(url_for("upload")) #if process of geting data from registration form is done then go to the page("upload")
+ 
+ return render_template("registration.html",form =obj_Class) # first this this is return becuse no any post request is get 
+    # IF ANY ERROR IS HAPPEN LIKE 
+    #             - Data is incomplete , not write post etc also this run or return Repetedly
+ 
+@app.route("/upload",methods =["POST","GET"])  #GET - to get the user form for image submission\
+                                              #POST - to post or send the image to the serve or python backend
+def upload():
+   return render_template("upload.html")
+
+
+
+
+
+
+@app.route("/result", methods= ["GET","POST"])
+def result():
+   return render_template("result.html")
+   
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm  #wht we use all function in flask form like validator and feild
+from wtforms import StringField,EmailField,SubmitField   #give the field to input the value
+from wtforms.validators import Email,ValidationError,DataRequired   #rules for specific feild
+
+class Datainput(FlaskForm) :   #need to extend th flaskform in class
+    name = StringField(label="Name" , validators= [DataRequired()])   #get the name from user in name python variable
+    email_id = EmailField(label="Enter the email :",validators=[DataRequired(),Email()])  #in email_id python variable # need to download email validator seaprate
+    submit= SubmitField("click to send Data and Goto upload") 
+ 
